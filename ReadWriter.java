5cb2824919d435f4489c93ae3d19788445d5f6ca
Daniel Bragg <114534607+NoBiscuit4U@users.noreply.github.com>
2026-03-04 09:50:52 -0600

    Add files via upload

--- ReadWriter.java
+++ ReadWriter.java
@@ -1,63 +1,85 @@
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class ReadWriter {
-	private String m_fp;
+	private static String m_root_url = "jdbc:mysql://localhost:3306/";
+	private String m_username;
+	private String m_password;
+	private String m_DBName;
 	
-	
-	public ReadWriter(String fp){
-		this.m_fp=fp;
-		
-		try(FileOutputStream fop=new FileOutputStream(m_fp);){
-			fop.close();
-		}catch(Exception e) {
-			System.out.println("NO FILE FOUND, ERROR: "+e);
-		}
+	public ReadWriter(String username,String password,String DBName){
+		this.m_username=username;
+		this.m_password=password;
+		this.m_DBName=DBName;
 	}
 	
-	public void appendFile(Object obj){
-		ArrayList<Object> old_objs=this.readFile();
-
-		try(ObjectOutputStream oop=new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(m_fp,true)))){
-			oop.writeObject(obj);
-
-			for(Object old_obj:old_objs){
-				oop.writeObject(old_obj);
-			}
-
-			oop.flush();	
-			oop.close();
-			
-		}catch(Exception e) {
-			System.out.println("WRITE OBJ FAILED, ERROR: "+e);
+	public Object runQuery(String query_type,String full_query,String target_table,boolean scope_all) {
+		switch(query_type) {
+			case "select":
+				return this.selectQuery(full_query,target_table,scope_all);
+			case "update":
+			break;
+			case "delete":
+			break;
+			case "insert into":
+			break;
 		}
+		
+		return new Object();
 	}
 	
-
-	public ArrayList<Object> readFile(){
-		ArrayList<Object> returnArr=new ArrayList<Object>();
+	private Object selectQuery(String full_query,String target_table,boolean scope_all) throws Exception{
+		Object returnObj=null;
 		
-			try(ObjectInputStream oip=new ObjectInputStream(new BufferedInputStream(new FileInputStream(m_fp)))){
-				while(true) {
-						try{
-							Object obj=oip.readObject();
-
-							if(obj instanceof Product){
-								returnArr.add((Product) obj);
-							}else {
-								returnArr.add((User) obj);
-							}
-						}catch (EOFException e) {
-							System.out.println("FINISHED "+e);
-							oip.close();
-							break;
-						}
-	
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(this.m_root_url+this.m_DBName,this.m_username,this.m_password);
+		System.out.println("Connection Established successfully");
+		Statement st=con.createStatement();
+		ResultSet rs=st.executeQuery(full_query);
+		rs.next();
+		
+		switch(target_table){
+		//Make Constants For Table Names
+			case "pdct":
+				if(scope_all) {
+					ArrayList<Object> bulk_return=new ArrayList<Object>();
+					
+					for(int i=0;i<rs.getFetchSize();i++) {
+						rs.next();
+						//Make Constants for results getting
+						bulk_return.add(new Product(rs.getString(""),rs.getInt(""),rs.getDouble(""),rs.getString("")));
+					}
+					
+					returnObj=bulk_return;
+				}else {
+					//Make Constants for results getting
+					returnObj=new Product(rs.getString(""),rs.getInt(""),rs.getDouble(""),rs.getString(""));
 				}
-			}catch(Exception e) {
-				System.out.println(e);
-			}
-			
-		return(returnArr);
+				
+				break;
+			case "usr":
+				if(scope_all) {
+					ArrayList<Object> bulk_return=new ArrayList<Object>();
+					
+					for(int i=0;i<rs.getFetchSize();i++) {
+						rs.next();
+						//Make Constants for results getting
+						bulk_return.add(new User(rs.getString(""),rs.getInt(""),rs.getString(""),rs.getString("")));
+					}
+					
+					returnObj=bulk_return;
+				}else {
+					//Make Constants for results getting
+					returnObj=new User(rs.getString(""),rs.getInt(""),rs.getString(""),rs.getString(""));
+				}
+				
+				break;
+		}
+		
+		return returnObj;
 	}
 }
