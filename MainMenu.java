227c6e6ff22c277588488c606b50f20d84a849e8
Daniel <razyback06@gmail.com>
2026-04-13 11:10:41 -0500

    Init for Main Menu and Login

---
+++ MainMenu.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu extends JFrame{
+    
+    MainMenu(){
+        GridBagConstraints gridConstraints=null;
+
+        setLayout(new GridBagLayout());
+    }
+}
